0f58c236e984361a9fcc3297cd637864e1954a6f
Aditya <171126285+Adityaaaaaaaaaaaaaaa@users.noreply.github.com>
2025-06-24 12:37:22 +0400

    inventory sort dropdown revamp

--- my_cooking_helper/lib/features/inventory/inventory_sort.dart
+++ my_cooking_helper/lib/features/inventory/inventory_sort.dart
@@ -10,6 +10,7 @@ class InventorySortBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Row(
       children: [
+        Padding(padding: EdgeInsets.symmetric(horizontal: 2.w, vertical: 2.h)),
         Text(
           "Sort by:", 
           style: TextStyle(
@@ -25,8 +26,14 @@ class InventorySortBar extends StatelessWidget {
               onSort(value);
             }
           },
+          dropdownColor: Colors.black.withOpacity(0.60),
+          icon: Icon(Icons.arrow_drop_down, color: Colors.white70,),
+          style: TextStyle(color: Colors.white, fontSize: 16.sp),
+          iconSize: 25.0.sp,
+          menuWidth: 100.w,
+          borderRadius: BorderRadius.circular(15.r),
           items: [
-            DropdownMenuItem(value: "default", child: Text("Default",style: TextStyle(fontWeight: FontWeight.w600,fontSize: 11.sp,))),
+            DropdownMenuItem(value: "default", child: Text("Default", style: TextStyle(fontWeight: FontWeight.w600,fontSize: 11.sp,))),
             DropdownMenuItem(value: "name", child: Text("Name", style: TextStyle(fontWeight: FontWeight.w600,fontSize: 11.sp,))),
             DropdownMenuItem(value: "quantity", child: Text("Quantity", style: TextStyle(fontWeight: FontWeight.w600,fontSize: 11.sp,))),
             DropdownMenuItem(value: "category", child: Text("Category", style: TextStyle(fontWeight: FontWeight.w600,fontSize: 11.sp,))),
